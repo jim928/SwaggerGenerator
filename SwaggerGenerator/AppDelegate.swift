538433b2cdd19ee434c0a0618811cb0215f1cfec
jim <>
2021-01-23 23:58:42 +0800

    create

---
+++ SwaggerGenerator/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SwaggerGenerator
+//
+//  Created by jim on 2021/1/23.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ SwaggerGenerator/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  SwaggerGenerator
+//
+//  Created by jim on 2021/1/23.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
